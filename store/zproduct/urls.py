cf091d5c0d6b660bfdba35f67e4c50d74450c31d
5tirner <thatfor1337@gmail.com>
2024-11-29 02:09:15 +0100

    Move restframework_simplejwt views to zproduct.urls

--- store/zproduct/urls.py
+++ store/zproduct/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import welcomePage, signin, login, activate, profile
 from .endpoints import setData, createVerification
+from rest_framework_simplejwt import views
 
 urlpatterns = [
+    #Views
     path('', welcomePage),
     path('sign', signin),
     path('log', login),
@@ -11,4 +13,8 @@ urlpatterns = [
     #ENDPOINTS
     path('setData', setData),
     path('verf', createVerification),
+    #AUTH
+    path('api/token/', views.TokenObtainPairView.as_view(), name='default'),
+    path('api/token/refresh/', views.TokenRefreshView.as_view(), name='refresh'),
+    path('api/token/verify/', views.TokenVerifyView.as_view(), name='verify'),
 ]
